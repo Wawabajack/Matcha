44b2229affa3e773ba725c17c7367eec29946a73
Corentin Chauvin <cchauvin@z3r6p2.le-101.fr>
2019-03-26 12:34:20 +0100

    registerview

--- pages/register.php
+++ pages/register.php
@@ -1,4 +1,32 @@
 <?php
+	require_once($_SERVER["DOCUMENT_ROOT"] . '/views/registerView.php');
+	require_once($_SERVER["DOCUMENT_ROOT"] . '/models/queryfuncs.php');
+	if (!isset($_SESSION))
+		session_start();
+?>
+
+<html>
+<head>
+	<meta name="viewport" content="width=device-width, initial-scale=1.0" charset="utf-8">
+	<meta content="Index" name="MatchaWeeb">
+	<link href="/css/main.css" rel="stylesheet" type="text/css">
+
+	<!------------ Bootstrap includes ------------>
+
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+
+
+	<!-----------------use--------------------------->
+
+</head>
+<body>
+<title>Register</title>
+<h1 class="hdr">MatchaWeeb</h1>
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+
+<?php
     require_once($_SERVER["DOCUMENT_ROOT"] .'/views/registerView.php');
 	require_once($_SERVER["DOCUMENT_ROOT"] . '/models/miscfuncs.php');
 
--- views/registerView.php
+++ views/registerView.php
@@ -1,12 +1,55 @@
 <?php
 
-    $regForm = '<form action="../controllers/register.php" method="post">
-    Username: <input type="text" name="username" required><br/>
-    Name: <input type="text" name="name" required><br/>
-    Surname: <input type="text" name="surname" required><br/>
-    Mail: <input type="email" autocomplete="mail" name="mail" required><br/>
-    Pass: <input type="password" name="pwd" autocomplete="current-password" required><br/>
-    <input type="submit" value="Go" name="register">
-</form>';
-
-    ?>
+    $regForm = '
+    <div class="container" class="regform">
+        <div class="d-flex justify-content-center h-100">
+            <div class="card">
+                <div class="card-header">
+                    <h3>Register</h3>
+                </div>
+                <div class="card-body">
+                    <form action="../controllers/register.php" method="post">
+                        <div class="input-group form-group">
+                            <div class="input-group-prepend">
+                                <span class=" input-group-text"><i class="fas fa-user"></i></span>
+                            </div>
+                            <input type="text" name="username" placeholder="username" required><br/>
+                        </div>
+                        <div class="input-group form-group">
+                            <div class="input-group-prepend">
+                                <span class=" input-group-text"><i class="fas fa-name"></i></span>
+                            </div>
+                            <input type="text" name="name" placeholder="name" required><br/>
+                        </div>
+                        <div class="input-group form-group">
+                            <div class="input-group-prepend">
+                                <span class=" input-group-text"><i class="fas fa-surname"></i></span>
+                            </div>
+                            <input type="text" name="surname" placeholder="surname" required><br/>
+                        </div>
+                        <div class="input-group form-group">
+                            <div class="input-group-prepend">
+                                <span class=" input-group-text"><i class="fas fa-mail"></i></span>
+                            </div>
+                            <input type="email" autocomplete="mail" placeholder="mail" name="mail" required><br/>
+                        </div>
+                        <div class="input-group form-group">
+                            <div class="input-group-prepend">
+                                <span class=" input-group-text"><i class="fas fa-pwd"></i></span>
+                            </div>
+                            <input type="password" autocomplete="current-password" placeholder="password" name="pwd" required><br/>
+                        </div>
+                        <div class="form-group">
+                            <input type="submit" value="Create" name="register">
+                        </div>
+                        <div class="card-footer">
+                            <div class="d-flex justify-content-center links">
+                                <a href="../index.php">acceuil</a>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>';
+?>
