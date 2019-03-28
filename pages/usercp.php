228381ebe28ceb5a618c7c40e36691a5ccc7cfb5
Corentin Chauvin <cchauvin@z3r6p2.le-101.fr>
2019-03-28 20:33:48 +0100

    bug mineur

--- pages/usercp.php
+++ pages/usercp.php
@@ -1,9 +1,10 @@
 <?php
-	require_once($_SERVER["DOCUMENT_ROOT"] . '/views/profileView.php');
 	require_once($_SERVER["DOCUMENT_ROOT"] . '/views/indexView.php');
 
 	if (!isset($_SESSION))
 		session_start();
+	if (isset($_SESSION['usr']))
+	require_once($_SERVER["DOCUMENT_ROOT"] . '/views/profileView.php');
 ?>
 
 <html>
@@ -23,6 +24,8 @@
 <body>
 <div id="notif"></div>
 <title>Mon profil</title>
+<div id="notif">erreur</div>
+<h1 class="hdr">MatchaWeeb</h1>
 <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
