ca6fd691274184413f35ca794dc0a1a732592c40
Corentin Chauvin <cchauvin@z3r6p2.le-101.fr>
2019-04-01 16:46:41 +0200

    map

--- models/setupfuncs.php
+++ models/setupfuncs.php
@@ -28,12 +28,12 @@
 	function create_markers_table($db) {
 		$sql = "DROP TABLE IF EXISTS `markers`;
         CREATE TABLE `markers` (
-        `id` int(11) PRIMARY KEY NOT NULL AUTO_INCREMENT,
-        `uid` int(11) NOT NULL UNIQUE,
-        `name` varchar(64) NOT NULL,
-		`lat` float(10,6) NOT NULL,
-		`ing` float(10,6) NOT NULL,
-        ) ENGINE=InnoDB DEFAULT CHARSET=utf8;";
+			`id` INT NOT NULL AUTO_INCREMENT PRIMARY KEY ,
+			`uid` int(11) NOT NULL UNIQUE,
+			`name` varchar(64) NOT NULL,
+			`lat` FLOAT( 10, 6 ) NOT NULL ,
+			`lng` FLOAT( 10, 6 ) NOT NULL
+		  ) ENGINE=InnoDB DEFAULT CHARSET=utf8;";
 		if (!isset($db))
 			echo 'Erreur lors de la création de la table des markers, veuillez vérifier la connection avec le serveur SQL' . '<br/>';
 		else {
