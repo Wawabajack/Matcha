ca5f8cb337a3e60ca4f16173eae8252001be5bf5
Baptiste Robichon <brobicho@z3r5p2.le-101.fr>
2019-04-03 20:40:02 +0200

    mr propre repare

--- views/profileView.php
+++ views/profileView.php
@@ -20,7 +20,7 @@ if (isset($_SESSION['usr'])) {
 /** Profile infos **/
 
 if (isset($_SESSION['profile'])) {
-    $gender = '' . $_SESSION['profile']->gender . '<br/>';
+    $gender = '' . $_SESSION['profile']->gender;
     if ($_SESSION['profile']->birthdate == "0000-00-00")
         $_SESSION['profile']->birthdate = "1970-01-01";
     $date = new DateTime($_SESSION['profile']->birthdate);
@@ -28,10 +28,10 @@ if (isset($_SESSION['profile'])) {
     $interval = $now->diff($date);
     $age = $interval->y;
     $birth = explode('-', $_SESSION['profile']->birthdate);
-    $birth = $birth[1] . '/' . $birth[2] . '/' . $birth[0];
+    $birth = $birth[2] . '/' . $birth[1] . '/' . $birth[0];
     $location = "";
-    if ($_SESSION['profile']->country != "")
-        $location = $_SESSION['profile']->city . ' ' . $_SESSION['profile']->region . ' ' . $_SESSION['profile']->country . '<br/>';
+    if ($_SESSION['profile']->city != "")
+        $location = $_SESSION['profile']->city;
     $popScore = '' . $_SESSION['profile']->popularity . '<br/>';
     $image = $_SESSION['profile']->img;
 } else {
